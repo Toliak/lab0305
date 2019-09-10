04a11c0d0ae80fea5dba0e0531528d67cd5cdc2f
Toliak <ibolosik@gmail.com>
2019-09-10 18:30:10 +0300

    Added test data

---
+++ tests/test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <boost/regex.hpp>
+
+TEST(Test, Test)
+{
+    boost::regex r{R"((\w+)_(\d{8})_([12]\d{3}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])).(\w+))"};
+    std::string tester{"builder_12345678_20190504.txt"};
+
+    EXPECT_EQ(
+        static_cast<bool>(boost::regex_match(tester, r)),
+        true
+    );
+}
